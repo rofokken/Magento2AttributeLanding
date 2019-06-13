8ece46b7af5490eaa7e050e4ceac58ff3b8a1aee
Edwin Jacobs <ejacobs@emico.nl>
2019-06-13 18:33:32 +0200

    Added filterhider plugin for navigation state

---
+++ src/Plugin/LayeredNavigation/Block/Navigation/State/FilterHidePlugin.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @author : Edwin Jacobs, email: ejacobs@emico.nl.
+ * @copyright : Copyright Emico B.V. 2019.
+ */
+
+namespace Emico\AttributeLanding\Plugin\LayeredNavigation\Block\Navigation\State;
+
+use Emico\AttributeLanding\Model\FilterHider\FilterHiderInterface;
+use Emico\AttributeLanding\Model\LandingPageContext;
+use Magento\Catalog\Model\Layer\Filter\FilterInterface;
+use Magento\LayeredNavigation\Block\Navigation\State;
+
+class FilterHidePlugin
+{
+    /**
+     * @var FilterHiderInterface
+     */
+    private $filterHider;
+
+    /**
+     * @var LandingPageContext
+     */
+    private $landingPageContext;
+
+    /**
+     * Plugin constructor.
+     * @param LandingPageContext $landingPageContext
+     * @param FilterHiderInterface $filterHider
+     */
+    public function __construct(
+        LandingPageContext $landingPageContext,
+        FilterHiderInterface $filterHider
+    ) {
+        $this->filterHider = $filterHider;
+        $this->landingPageContext = $landingPageContext;
+    }
+
+    /**
+     * @param State $subject
+     * @param $filters
+     * @return mixed
+     */
+    public function afterGetActiveFilters(State $subject, array $filterItems)
+    {
+        $landingPage = $this->landingPageContext->getLandingPage();
+        if (!$landingPage || $landingPage->getHideSelectedFilters()) {
+            return $filterItems;
+        }
+        /**
+         * @var int $index
+         * @var FilterInterface $filterItem
+         */
+        foreach ($filterItems as $index => $filterItem) {
+            if ($this->filterHider
+                ->shouldHideFilter($landingPage, $filterItem->getFilter())
+            ) {
+                unset($filterItems[$index]);
+            }
+        }
+
+        return $filterItems;
+    }
+}
